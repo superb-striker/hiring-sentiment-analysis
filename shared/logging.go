6a75155ddbe5a189630b6a8164b725bf11b12a2e
Nipun Kothari <nipunkothari04@gmail.com>
2026-08-10 23:29:01 +0530

    feat(shared): added generic logger for all modules

---
+++ shared/logging.go
@@ -0,0 +1,13 @@
+package contracts
+
+import (
+	"log/slog"
+	"os"
+)
+
+func NewLogger(service string) *slog.Logger {
+	handler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelInfo,
+	})
+	return slog.New(handler).With("service", service)
+}
